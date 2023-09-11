455e1f35b1ff293a0aba5ebf6af20f0aa3f1ead2
lionelkvn <lionel.natividad@gmail.com>
2023-09-12 03:34:37 +0800

    home-slider

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -4,7 +4,7 @@ const Home = () => {
   return (
       <div id="Home" className="Home-container">
             <div class="HomeImage">
-                <img src="./slideImage.png" alt="Home Slider"/>
+                <img src="./slideImage.png" alt="Home-Slider"/>
             </div>
             <div class='HomeContent'>
                 <h1><span class="primary-color">Ink</span><span>novative</span></h1> 
